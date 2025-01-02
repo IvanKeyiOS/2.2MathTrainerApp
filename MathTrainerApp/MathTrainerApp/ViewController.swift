8973cd7f3a3f561cc52d8aca9210647430f920f0
IvanKeyiOS <ivanbeat888@gmail.com>
2025-01-02 11:03:42 +0100

    add new project

---
+++ MathTrainerApp/MathTrainerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MathTrainerApp
+//
+//  Created by Иван Курганский on 02/01/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
